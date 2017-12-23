080c5489d3c91f64cf4662c080f8d10be958509e
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2017-12-23 02:14:56 +0100

    Add files via upload

---
+++ 7.2_Filip Jachura.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+void drukuj(char*tekst, int*tab)
+{
+  printf("Wynik dzialania funkcji printf+scanf: \n");
+  int i =0;
+  while(*tekst)
+  {
+    if(*tekst=='%')
+    {
+      tekst++;
+      if(*tekst=='d')
+      {
+        printf("%d",*tab+i);
+        i++;
+        tekst++;
+      }
+      else
+      {
+      tekst--;
+        printf("%c",*tekst);
+      tekst++;
+      }
+    }
+    printf("%c",*tekst);
+    tekst++;
+
+  }
+}
+void wpisz(char* tekst)
+{
+  char* kopia = tekst;
+  int a[100];
+  int i =0;
+  while(*tekst)
+  {
+    if(*tekst=='%')
+    {
+      tekst++;
+      if(*tekst=='d')
+      {
+        scanf("%d",&a[i]);
+        i++;
+        tekst++;
+      }
+      else
+      {
+      tekst--;
+        printf("%c",*tekst);
+      tekst++;
+      }
+    }
+    printf("%c",*tekst);
+    tekst++;
+
+  }
+  printf("\n\n\n");
+  drukuj(kopia,a);
+}
+int main()
+{
+  wpisz("Dlugosc pierwszego boku: %d\nDlugosc drugiego boku: %d");
+  return 0;
+}
