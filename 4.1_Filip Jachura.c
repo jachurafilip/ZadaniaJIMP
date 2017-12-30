19ead3105a5074e9f9632381251c681964278ea6
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2017-12-30 01:13:30 +0100

    Update 4.1_Filip Jachura.c

--- 4.1_Filip Jachura.c
+++ 4.1_Filip Jachura.c
@@ -4,23 +4,26 @@
 
 double delta(double a, double b, double c)
 {
-    return b*b-4*a*c;
+  return b*b-4*a*c;
 }
 
 void liczPierwiaski(double a, double b, double c)
 {
-    if(delta(a,b,c)<0)
+  if(delta(a,b,c)<0)
     {
-        printf("Nie ma pierwiastkow rzeczywistych");
-
+      printf("Nie ma pierwiastkow rzeczywistych");
     }
     else if(delta(a,b,c) == 0)
     {
-        printf("Jedynym pierwiastkiem rownania jest liczba %lf",-b/2*a);
+      int pierwiastek=-b/(2*a)
+      printf("Jedynym pierwiastkiem rownania jest liczba %lf",pierwiastek);
     }
     else
     {
-        printf("Istnieja dwa piewiastki: %lf i %lf",(-b+pow(delta(a,b,c),0.5))/(2*a),(-b-pow(delta(a,b,c),0.5))/(2*a));
+      int pierwiastek1=(-b+pow(delta(a,b,c),0.5))/(2*a);
+      int pierwiastek2=(-b-pow(delta(a,b,c),0.5))/(2*a);
+
+      printf("Istnieja dwa piewiastki: %lf i %lf",pierwiastek1,pierwiastek2);
     }
 }
 
@@ -29,6 +32,11 @@ int main()
     double a,b,c;
     printf("Podaj wspolczynniki: \na=");
     scanf("%lf",&a);
+    while(a==0)
+    {
+      printf("Rownanie nie jest kwadratowe. Podaj a rozne od 0:\n");
+      scanf("%lf".&a);
+    }
     printf("b=");
     scanf("%lf",&b);
     printf("c=");
