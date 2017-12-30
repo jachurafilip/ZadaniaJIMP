cce3c34816437bcdd79a354342d6793ace91e3c6
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2017-12-30 01:15:55 +0100

    Update 4.2_Filip Jachura.c

--- 4.2_Filip Jachura.c
+++ 4.2_Filip Jachura.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
-
 void zgadnij (int n){
 	printf("Twoja liczba to: ");
-	if (n>5){
-		if (n>7){
-	    	    if (n>9) printf("10");
-                    else if (n>8) printf("9");
-                    else printf("8");
-		}
-		else if (n>6) printf("7");
-        	else printf("6");
-	}
-    	else if (n>2)
+	if (n>5)
+  {
+		if (n>7)
 		{
-		if (n>4) printf("5");
-		else if (n>3) printf("4");
-		else printf("3");
+      if (n>9)
+        printf("10");
+      else if (n>8)
+        printf("9");
+      else
+        printf("8");
 		}
-	else if (n>1) printf("2");
-        else printf("1");
+		else if (n>6)
+      printf("7");
+  else
+    printf("6");
+	}
+    else if (n>2)
+		{
+		if (n>4)
+      printf("5");
+		else if (n>3)
+      printf("4");
+		else
+      printf("3");
+		}
+	else if (n>1)
+    printf("2");
+        else
+          printf("1");
 	printf ("\n");
 }
-int sprawdz (int n){
-	if (n>10){
-		n=n%10;
-		printf("Twoja liczba była wieksza niz 10. Twoja nowa liczba to ostatnia cyfra starej\n");
-	}
-	else if (n==0){
-		n+=1;
-		printf("Twoja liczba byla rowna 0. Zamienilem ja na 1\n");
-	}
-	else if (n<0){
-		n=(-n)%10;
-		printf("Twoja liczba była ujemna. Twoja nowa liczba to ostatnia cyfra starej\n");
-	}
-	return n;
+
+int sprawdz (int n)
+{
+	while(n<1 || n>10)
+  {
+    printf("Podaj liczbe z zakresu [1,10]: \n");
+    scanf("%d",&n);
+  }
+  return n;
 }
 
 int main (void){
@@ -45,7 +51,3 @@ int main (void){
 	zgadnij (n);
 	return 0;
 }
-
-
-
-
