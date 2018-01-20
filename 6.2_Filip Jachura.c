4144a030280407f6acfdb4aa1344b3fd7a450daa
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-01-20 18:20:56 +0100

    Update 6.2_Filip Jachura.c

--- 6.2_Filip Jachura.c
+++ 6.2_Filip Jachura.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 #include<time.h>
 
-const int dni[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+int dni[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 
-void rysuj(int miesiac, int dzienmies, int dzientyg)
+int przestepny(int rok)
+{
+  if(rok%4 == 0)
+  {
+    if(rok%100 == 0)
+    {
+      if (rok%400 == 0)
+        return 1;
+      return 0;
+    }
+    return 0;
+    }
+    return 1;
+}
+void rysuj(int rok, int miesiac, int dzienmies, int dzientyg)
 {
   int format=(dzientyg-dzienmies+49)%7;
   printf("PON WTO SRO CZW PIA SOB NIE\n");
@@ -13,6 +27,10 @@ void rysuj(int miesiac, int dzienmies, int dzientyg)
   }
   for (int i =1; i<=dni[miesiac];i++)
   {
+    if(przestepny(rok))
+    {
+      dni[2]+=1;
+    }
     if(i<10)
     {
       printf(" ");
@@ -31,8 +49,10 @@ int main()
   struct tm czas;
   time(&sekundy);
   localtime_s(&czas,&sekundy);
+  
   char tytul[20];
   strftime(tytul,20,"%B %Y", &czas);
+  
   printf("\t%s\n",tytul);
-  rysuj(czas.tm_mon+1,czas.tm_mday,czas.tm_wday%7);
+  rysuj(czas.tm_year, czas.tm_mon+1,czas.tm_mday,czas.tm_wday%7);
 }
