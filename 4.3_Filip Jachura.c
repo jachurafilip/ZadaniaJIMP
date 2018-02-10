5c7b5e3ebe6f1cb4f24514cb876f1219c7a6497b
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-10 20:20:04 +0100

    Update 4.3_Filip Jachura.c

--- 4.3_Filip Jachura.c
+++ 4.3_Filip Jachura.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 int losujLiczbe()
 {
     return rand()%9;
 }
 
-void stworzTablice(int tab[],int rozmiar)
+void stworzTablice(int tab[], int rozmiar)
 {
   for (int i=0; i<rozmiar; i++)
   {
@@ -22,7 +23,7 @@ void wybierzKulki(int tab[], int ile)
   printf("Podaj %d numerow kulek, ktore chcesz zwazyc (z przedzialu 0-8): \n",ile);
   for (int i=0; i<ile; i++)
   {
-    scanf("%d",&tmp);
+    scanf("%d", &tmp);
 
     while(tmp>8)
     {
@@ -37,12 +38,11 @@ void Wazenie1(int kulki[], int mojeKulki[])
   int lewa = kulki[mojeKulki[0]]+kulki[mojeKulki[1]]+kulki[mojeKulki[2]];
   int prawa = kulki[mojeKulki[3]]+kulki[mojeKulki[4]]+kulki[mojeKulki[5]];
 
-  if(lewa==prawa)
+  if(lewa == prawa)
   {
     printf("Kulki %d, %d i %d waza tyle samo co %d, %d i %d\n",mojeKulki[0],mojeKulki[1],mojeKulki[2],mojeKulki[3],mojeKulki[4],mojeKulki[5]);
   }
-
-  else if(lewa>prawa)
+  else if(lewa > prawa)
   {
     printf("Kulki %d, %d i %d sa ciezsze niz %d, %d i %d\n",mojeKulki[0],mojeKulki[1],mojeKulki[2],mojeKulki[3],mojeKulki[4],mojeKulki[5]);
   }
@@ -57,11 +57,11 @@ void Wazenie2(int kulki[], int mojeKulki[])
   int lewa = kulki[mojeKulki[0]];
   int prawa = kulki[mojeKulki[1]];
 
-  if (lewa==prawa)
+  if (lewa == prawa)
   {
     printf("Kulki %d i %d waza tyle samo\n",mojeKulki[0],mojeKulki[1]);
   }
-  else if (lewa>prawa)
+  else if (lewa > prawa)
   {
     printf("Kulka %d jest ciezsza od %d\n",mojeKulki[0],mojeKulki[1]);
   }
@@ -75,7 +75,7 @@ int tabMax(int tab[], int dlugosc)
   int max,indeks;
   for (int i =0; i<dlugosc; i++)
   {
-    if(tab[i]>max)
+    if(tab[i] > max)
     {
       max = tab[i];
       indeks = i;
@@ -92,16 +92,16 @@ int main(void)
   stworzTablice(kulki,sizeof(kulki)/sizeof(kulki[0]));
 
   int wybraneKulki1[6];
-  wybierzKulki(wybraneKulki1,sizeof(wybraneKulki1)/sizeof(wybraneKulki1[0]));
-  Wazenie1(kulki,wybraneKulki1);
+  wybierzKulki(wybraneKulki1, sizeof(wybraneKulki1)/sizeof(wybraneKulki1[0]));
+  Wazenie1(kulki, wybraneKulki1);
 
   int wybraneKulki2[2];
-  wybierzKulki(wybraneKulki2,sizeof(wybraneKulki2)/sizeof(wybraneKulki2[0]));
-  Wazenie2(kulki,wybraneKulki2);
+  wybierzKulki(wybraneKulki2, sizeof(wybraneKulki2)/sizeof(wybraneKulki2[0]));
+  Wazenie2(kulki, wybraneKulki2);
 
   printf("Podaj ktora kulka jest najciezsza:\n");
   int najciezsza;
-  scanf("%d",&najciezsza);
+  scanf("%d", &najciezsza);
 
   if(najciezsza == tabMax(kulki,9))
   {
