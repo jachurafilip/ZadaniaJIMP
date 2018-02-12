4bb748d1901a24a640fb13156f215b4cb9e77d5c
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-12 12:01:04 +0100

    Update 5.1_Filip Jachura.c

--- 5.1_Filip Jachura.c
+++ 5.1_Filip Jachura.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 
-int porownaj (int czas1 [3], int czas2 [3])
+int porownaj (int czas1 [], int czas2 [],int rozmiar)
 {
-  for (int i=0; i<3; i++)
+  for (int i = 0; i < rozmiar; i++)
   {
-    if (czas1[i]>czas2[i])
+    if (czas1[i] > czas2[i])
       return 1;
-    if (czas1[i]<czas2[i])
+    if (czas1[i] < czas2[i])
       return -1;
   }
   return 0;
 }
-int sprawdzCzas(int czas[3])
+
+int sprawdzCzas(int czas[])
 {
-  if (czas[0]>23||czas[0]<0)
+  if (czas[0] > 23 || czas[0] < 0)
     return 0;
-  if (czas[1]>59||czas[1]<0)
+  if (czas[1]>59 || czas[1] < 0)
     return 0;
-  if (czas[2]>59||czas[2]<0)
+  if (czas[2]>59 || czas[2]<0)
     return 0;
   return 1;
 }
 
-void podajCzas(int czas[3])
+void podajCzas(int czas[])
 {
   printf("Podaj godzine\n");
   scanf ("%d%d%d", czas, czas+1,czas+2);
@@ -30,23 +31,24 @@ void podajCzas(int czas[3])
 
 int main(void)
 {
-  int czas1 [3];
-  int czas2 [3];
+  int rozmiar = 3;
+  int czas1[rozmiar];
+  int czas2[rozmiar];
 
   podajCzas(czas1);
   podajCzas(czas2);
 
-  if(!(sprawdzCzas(czas1)&&sprawdzCzas(czas2)))
+  if(!(sprawdzCzas(czas1) && sprawdzCzas(czas2)))
   {
     printf ("Nieprawidlowy format. Uzyj hh:mm:ss\n");
     return 0;
   }
 
-  if (porownaj (czas1, czas2)==1)
+  if (porownaj (czas1, czas2, rozmiar) == 1)
   {
     printf("Pierwsza wprowadzona godzina jest pozniejsza niz druga");
   }
-  else if (porownaj(czas1,czas2)==0)
+  else if (porownaj(czas1, czas2, rozmiar) == 0)
   {
     printf("Godziny sa identyczne");
   }
@@ -54,6 +56,5 @@ int main(void)
   {
     printf("Druga wprowadzona godzina jest pozniejsza niz pierwsza");
   }
-
   return 0;
 }
