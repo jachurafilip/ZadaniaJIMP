935b2f6c39ef27b06c4a650a5c83809f3c6237eb
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-12 12:05:58 +0100

    Update 6.1_Filip Jachura.c

--- 6.1_Filip Jachura.c
+++ 6.1_Filip Jachura.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 void wypiszASCII()
 {
-  char i=0;
-  for (int j=0; j<256; j++)
+  char i = 0;
+  for (int j = 0; j < 256; j++)
   {
-    printf("%c\t", i); //wypisuje znak ASCII o danym kodzie
+    printf("%c\t", i);
     printf("%d\n",j);
     i++;
   }
