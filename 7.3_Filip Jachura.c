7ddc8dd34211fd64bb82984f340c57900f0cedb8
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-12 12:23:04 +0100

    Update 7.3_Filip Jachura.c

--- 7.3_Filip Jachura.c
+++ 7.3_Filip Jachura.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int compare(const void** _a, const void** _b)
 {
   int* a = *(int**)_a;
@@ -7,7 +8,7 @@ int compare(const void** _a, const void** _b)
 
   int liczba1 = *a;
   int liczba2 = *b;
-  return liczba1-liczba2;
+  return liczba1 - liczba2;
 }
 
 
@@ -15,23 +16,25 @@ int main()
 {
 
     int tab[] = {1,3,5,7,9,2,4,6,8,10};
+
     int* wsk[10];
+
     for(int i=0; i<10;i++)
     {
       wsk[i]=&tab[i];
     }
-    qsort(wsk,10,sizeof(int*),compare); //korzystam z funkcji qsort
+    qsort(wsk, 10 ,sizeof(int*), compare);
+
     printf("Tablica wyswietlona bezposrednio: \n");
-    for(int i=0; i<10;i++)
+    for(int i=0; i < 10; i++)
     {
-      printf("%d ",tab[i]);
+      printf("%d ", tab[i]);
     }
     printf("\n");
     printf("Tablica wyswietlona poprzez posortowane wskazniki: \n");
-    for(int k=0; k<10;k++)
+    for(int k=0; k<10; k++)
     {
-      printf("%d ",*wsk[k]);
+      printf("%d ", *wsk[k]);
     }
-
     return 0;
 }
