782e97b3efa7e58c005982820ee3eb8414e20578
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-12 12:21:45 +0100

    Update 7.2_Filip Jachura.c

--- 7.2_Filip Jachura.c
+++ 7.2_Filip Jachura.c
@@ -1,61 +1,64 @@
 #include <stdio.h>
-void drukuj(char*tekst, int*tab)
+
+void drukuj(char* tekst, int* tab)
 {
   printf("Wynik dzialania funkcji printf+scanf: \n");
-  int i =0;
+  int i = 0;
   while(*tekst)
   {
-    if(*tekst=='%')
+    if(*tekst == '%')
     {
       tekst++;
-      if(*tekst=='d')
+      if(*tekst == 'd')
       {
-        printf("%d",*tab+i);
-        i++;
+        printf("%d", *tab+i);
+        tab++;
         tekst++;
       }
       else
       {
-      tekst--;
-        printf("%c",*tekst);
-      tekst++;
+        tekst--;
+        printf("%c", *tekst);
+        tekst++;
       }
     }
-    printf("%c",*tekst);
+    printf("%c", *tekst);
     tekst++;
 
   }
 }
+
 void wpisz(char* tekst)
 {
   char* kopia = tekst;
   int a[100];
-  int i =0;
+  int i = 0;
   while(*tekst)
   {
-    if(*tekst=='%')
+    if(*tekst == '%')
     {
       tekst++;
-      if(*tekst=='d')
+      if(*tekst == 'd')
       {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
         i++;
         tekst++;
       }
       else
       {
-      tekst--;
-        printf("%c",*tekst);
-      tekst++;
+        tekst--;
+        printf("%c", *tekst);
+        tekst++;
       }
     }
-    printf("%c",*tekst);
+    printf("%c", *tekst);
     tekst++;
 
   }
   printf("\n\n\n");
-  drukuj(kopia,a);
+  drukuj(kopia, a);
 }
+
 int main()
 {
   wpisz("Dlugosc pierwszego boku: %d\nDlugosc drugiego boku: %d");
