0e1544e9933043beab6081a437c2baedc09d0e03
jachurafilip <33846600+jachurafilip@users.noreply.github.com>
2018-02-12 12:13:46 +0100

    Update 7.1_Filip Jachura.c

--- 7.1_Filip Jachura.c
+++ 7.1_Filip Jachura.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-
 float srednia(int* pocz, int* kon)
 {
-  float suma =0;
-  int waga=0;
-  for (int i =0;i<kon-pocz;i++)
+  float suma = 0;
+  int waga = 0;
+  for (int i = 0; i < (kon - pocz); i++)
   {
-    suma+=(*(pocz+i)*i); // element tablicy mno¿ymy przez jego wagê
-    waga+=i; //obliczamy sume wag
-  }
-  return suma/waga; // srednia wazona
+    suma += (*(pocz+i)*i);
+    waga += i;
+
+  return suma/waga;
 }
 
 int main()
 {
   int tab[]={3,5,6,3,4,5,1,2,4,7,6};
-  int rozmiar = (sizeof(tab)/sizeof(tab[0]));
-  float avg = srednia(tab,tab+rozmiar);
+
+  int rozmiar = (sizeof(tab) / sizeof(tab[0]));
+  float avg = srednia(tab, tab + rozmiar);
   printf("%f",avg);
   return 0;
 }
